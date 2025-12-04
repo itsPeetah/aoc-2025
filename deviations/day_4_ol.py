21d963fcf2f7a3bee0e6c60e49ef6737be5d4ba4
itsPeetah <pietro.g.moroni@gmail.com>
2025-12-04 09:49:05 +0100

    Update day_4_ol.py

--- deviations/day_4_ol.py
+++ deviations/day_4_ol.py
@@ -1,41 +1,18 @@
-def parse(path: str) -> tuple[list[int], int]:
-    line_length = 0
-    paper_rolls = []
-    with open(path) as file:
-        for line in file:
-            line = line.strip()
-            line_length = len(line)
-            paper_rolls.extend([0 if c == "." else 1 for c in line])
-    return paper_rolls, line_length
 
-def one_line_no_parsing(rolls: list[int], columns: int) -> int:
-    # return sum([
-    #     1 if (rolls[columns * y + x] if (x >= 0 and x < columns) and (y >= 0 and y < len(rolls) // columns) else 0) > 0
-    #     and sum([(rolls[columns * (y+j) + (x+i)] if ((x+i) >= 0 and (x+i) < columns) and ((y+j) >= 0 and (y+j) < len(rolls) // columns) else 0) for i, j in [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]]) < 4
-    #     else 0 for y, x in [ (y, x) for x in [x for x in range(columns )] for y in range(len(rolls) // columns)]
-    # ])
-    return sum([1 if (rolls[columns * y + x] if (x >= 0 and x < columns) and (y >= 0 and y < len(rolls) // columns) else 0) > 0 and sum([(rolls[columns * (y+j) + (x+i)] if ((x+i) >= 0 and (x+i) < columns) and ((y+j) >= 0 and (y+j) < len(rolls) // columns) else 0) for i, j in [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]]) < 4 else 0 for y, x in [ (y, x) for x in [x for x in range(columns )] for y in range(len(rolls) // columns)]])
+# Explained for readability
+accessible_rolls = [
+    sum([
+        1 # roll is accessible
+        if (rolls[columns * y + x] if (x >= 0 and x < columns) and (y >= 0 and y < len(rolls) // columns) else 0) > 0 # if the item is a roll (@)
+        and sum([(rolls[columns * (y+j) + (x+i)] if ((x+i) >= 0 and (x+i) < columns) and ((y+j) >= 0 and (y+j) < len(rolls) // columns) else 0) for i, j in [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]]) < 4 # if the roll has less than 4 neighbours
+        else 0 # roll is not accessible
+        for y, x in [ (y, x) for x in [x for x in range(columns )] for y in range(len(rolls) // columns)] # loop over the entire array using cols and rows instead of index
+    ]) # sum result (i.e. the accessible rolls)
+        for rolls, columns # use a generator to make the parsing result available to the sum without having to parse the input every time they're needed
+        in [([0 if c == "." else 1 for c in open("input/day4.txt").read().strip() if c != "\n"], open("input/day4.txt").read().find("\n"))] # parsing (array of rolls and row length)
+    ][0] # access only result and no parsing result
 
-rolls, row_len = parse("input/day4.txt")
-print(
-    "One line no parsing:",
-    one_line_no_parsing(rolls, row_len) == 1480 # real solution
-)
 
-def one_line_with_parsing() -> int:
-    # return [sum([
-    #     1 if (rolls[columns * y + x] if (x >= 0 and x < columns) and (y >= 0 and y < len(rolls) // columns) else 0) > 0
-    #     and sum([(rolls[columns * (y+j) + (x+i)] if ((x+i) >= 0 and (x+i) < columns) and ((y+j) >= 0 and (y+j) < len(rolls) // columns) else 0) for i, j in [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]]) < 4
-    #     else 0 for y, x in [ (y, x) for x in [x for x in range(columns )] for y in range(len(rolls) // columns)]
-    # ]) for rolls, columns in [([0 if c == "." else 1 for c in open("input/day4.txt").read().strip() if c != "\n"], open("input/day4.txt").read().find("\n"))]][0]
-    return [sum([1 if (rolls[columns * y + x] if (x >= 0 and x < columns) and (y >= 0 and y < len(rolls) // columns) else 0) > 0 and sum([(rolls[columns * (y+j) + (x+i)] if ((x+i) >= 0 and (x+i) < columns) and ((y+j) >= 0 and (y+j) < len(rolls) // columns) else 0) for i, j in [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]]) < 4 else 0 for y, x in [ (y, x) for x in [x for x in range(columns )] for y in range(len(rolls) // columns)]]) for rolls, columns in [([0 if c == "." else 1 for c in open("input/day4.txt").read().strip() if c != "\n"], open("input/day4.txt").read().find("\n"))]][0]
-    
+o=[sum([1 if(r[c*y+x]if(x>=0 and x < c)and(y>=0 and y<len(r)//c)else 0)>0 and sum([(r[c*(y+j)+(x+i)]if((x+i)>=0 and(x+i)<c)and((y+j)>=0 and(y+j)<len(r)//c)else 0)for i,j in[(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]])<4 else 0 for y,x in[(y,x)for x in[x for x in range(c)]for y in range(len(r)//c)]])for r,c in[([0 if c=="."else 1 for c in open("input/day4.txt").read().strip()if c!="\n"],open("input/day4.txt").read().find("\n"))]][0]
 
-print(
-    "One line with parsing:",
-    one_line_with_parsing() == 1480 # real solution
-)
-
-o = [sum([1 if (r[c * y + x] if (x >= 0 and x < c) and (y >= 0 and y < len(r) // c) else 0) > 0 and sum([(r[c * (y+j) + (x+i)] if ((x+i) >= 0 and (x+i) < c) and ((y+j) >= 0 and (y+j) < len(r) // c) else 0) for i, j in [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]]) < 4 else 0 for y, x in [ (y, x) for x in [x for x in range(c)] for y in range(len(r) // c)]]) for r, c in [([0 if c == "." else 1 for c in open("input/day4.txt").read().strip() if c != "\n"], open("input/day4.txt").read().find("\n"))]][0]
-
-print("Truly one line:", o,  o == 1480)
+print("Expected result:", 1480, "\nResult:", o)
