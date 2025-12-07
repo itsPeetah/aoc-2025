bdfad3d4bf31ff0c164f67dde83df3eab6abf9c9
itsPeetah <pietro.g.moroni@gmail.com>
2025-12-07 16:12:11 +0100

    day 7 part 2

--- day_7.py
+++ day_7.py
@@ -1,7 +1,7 @@
 TILE_VOID = 0
-TILE_START = 1
-TILE_SPLITTER = 2
-TILE_BEAM = 3
+TILE_START = -1
+TILE_SPLITTER = -2
+TILE_BEAM = 1
 
 
 def parse(path: str) -> list[list[int]]:
@@ -21,36 +21,39 @@ def parse(path: str) -> list[list[int]]:
     return world
 
 
-def advance_line(current_line_index: int, world: list[list[int]]) -> int:
-    def update_cell(line_idx: int, cell_idx: int, value: int) -> None:
-        if 0 <= cell_idx < len(world[line_idx]):
-            world[line_idx][cell_idx] = value
-
+def advance_beams(current_line_index: int, world: list[list[int]]) -> int:
     splits_counter = 0
     previous_line = world[current_line_index - 1]
     current_line = world[current_line_index]
     for i in range(len(current_line)):
         p = previous_line[i]
-        c = current_line[i]
         if p == TILE_START:
-            update_cell(current_line_index, i, TILE_BEAM)
-        elif p == TILE_BEAM:
-            if c == TILE_VOID:
-                update_cell(current_line_index, i, TILE_BEAM)
-            elif c == TILE_SPLITTER:
-                update_cell(current_line_index, i - 1, TILE_BEAM)
-                update_cell(current_line_index, i + 1, TILE_BEAM)
-                splits_counter += 2
+            current_line[i] = TILE_BEAM
+        elif p > 0:
+            if current_line[i] == TILE_SPLITTER:
+                splits_counter += 1
+                current_line[i - 1] += previous_line[i]
+                current_line[i + 1] += previous_line[i]
+            elif current_line[i] == TILE_VOID or current_line[i] >= TILE_BEAM:
+                current_line[i] += previous_line[i]
+
     return splits_counter
 
 
 def part1(world: list[list[int]]):
     cum_sum = 0
     for i in range(1, len(world)):
-        cum_sum += advance_line(i, world)
+        cum_sum += advance_beams(i, world)
     return cum_sum
 
 
-world = parse("input/day7.txt")
-p1 = part1(world)
+def part2(world: list[list[int]]) -> int:
+    for i in range(1, len(world)):
+        advance_beams(i, world)
+    return sum(world[-1])
+
+
+p1 = part1(parse("input/day7.txt"))
+p2 = part2(parse("input/day7.txt"))
 print("Total splits for part 1:", p1)
+print("Total timelines for part 2:", p2)
